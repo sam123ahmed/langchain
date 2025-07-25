5eeee599075dbb610666980ad2d86753723b6d9b
sameer91ahmed <136497604+sameer91ahmed@users.noreply.github.com>
2025-07-26 00:35:38 +0530

    delete zip

--- zip/20.Document_Loader/5_CSV_Loader/1_csv_loader.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import CSVLoader
-
-loader = CSVLoader(file_path="./20.Document_Loader/5_CSV_Loader/Social_Network_Ads.csv")
-
-## use lazy_load while deal with large amount of data
-docs = loader.load()
-
-print(docs[0])
-print(len(docs))
--- zip/6.Typeddict/1_typeddict_demo.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-
-class Person(TypedDict):
-    name: str
-    age: int
-
-new_person: Person = {"name": "nitish", "age": 25}
-
-print(new_person)
